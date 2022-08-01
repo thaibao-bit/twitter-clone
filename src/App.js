c95d6197b295017ca58d98626f2a0116ef810fd9
thaibao-bit <thaibaov2.0@gmail.com>
2022-08-01 16:42:36 +0700

    Title changed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { Route,Routes } from 'react-router-dom';
 import NavPage from './pages/navpage';
 import SignIn from './pages/signin';
 import SignInForm from './component/signinform';
+import { Helmet } from 'react-helmet';
 function App() {
   return (
     
     <div className="App">
+      <Helmet>
+      <title>Qitter. It's what happening / Qitter</title>
+      </Helmet>
+      
       <Routes>
       <Route path='/navpage' element= {<NavPage/>}></Route>
       <Route path='/form' element= {<SignInForm/>}></Route>
